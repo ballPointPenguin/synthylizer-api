4020a17807de9d41bc4513ad50a2e3d1f7ee3d57
Benjamin Rosas <ben@aliencyb.org>
2021-09-29 11:01:00 -0500

    install absinthe

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Synthylizer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # provided by mix phx.new:
       {:phoenix, "~> 1.6.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
@@ -48,7 +49,10 @@ defmodule Synthylizer.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      # added by me:
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
