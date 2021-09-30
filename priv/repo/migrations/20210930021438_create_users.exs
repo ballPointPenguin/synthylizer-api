ad476c349f10a3e1495a562925630658f8ccc567
Benjamin Rosas <ben@aliencyb.org>
2021-09-29 21:30:10 -0500

    modify user migration

--- priv/repo/migrations/20210930021438_create_users.exs
+++ priv/repo/migrations/20210930021438_create_users.exs
@@ -3,11 +3,13 @@ defmodule Synthylizer.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :username, :string
-      add :email, :string
-      add :password_hash, :string
+      add :username, :string, null: false
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
 
       timestamps()
     end
+
+    create unique_index(:users, [:username, :email])
   end
 end
