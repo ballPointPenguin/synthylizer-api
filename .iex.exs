72ac93beb76c9d9412bfbc08333944d99729c84d
Benjamin Rosas <ben@aliencyb.org>
2021-09-29 21:30:19 -0500

    include .iex.exs file

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+alias Synthylizer.Instruments.{Synthesizer}
+alias Synthesizer.Accounts.User
+
+alias Synthylizer.Repo
+alias Synthylizer.Instruments
+alias Synthylizer.Accounts
+
+import Ecto.Query
